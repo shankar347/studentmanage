d717bf7d224dea0d6a350638d360099189d88feb
sankarviacode <sivaaadi96@gmail.com>
2024-09-13 14:51:12 +0530

    new changes

--- backend/server.js
+++ backend/server.js
@@ -49,13 +49,13 @@ app.use('/api/message',messagerouter)
 // const __dirname=path.resolve()
 
 if(process.env.NODE_ENV === "production")
-{
-    app.use(express.static(path.join(__dirname,'frontend/dist')))
-
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'frontend','dist','index.html'))
-    })
-}
+    {
+        app.use(express.static(path.join(__dirname,'/frontend/dist')))
+            
+        app.get('*',(req,res)=>{
+         res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+        })
+    }
 
 
 server.listen(port,()=>{
