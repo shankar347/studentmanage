7753897f015586ccc6d4175d9aebe912ec824337
sankarviacode <sivaaadi96@gmail.com>
2024-09-16 11:25:17 +0530

    new changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,11 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{
-    port:3000,
+    port:4000,
     proxy:{
       '/api':{
-
-        target:'http://localhost:3000',
+      target:'http://localhost:3000',
       changeOrigin:true,
       secure:false
       }
